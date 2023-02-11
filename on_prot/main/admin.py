3e257f54440f1c55fc03ee8fd4cbd40395168f84
babs1987 <erlinyuriy@gmail.com>
2023-02-11 11:23:26 +0700

    Возобновление
    связал модуль турнирной сетки с шаблоном

---
+++ on_prot/main/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Armwrestler,Competition
+# Register your models here.
+admin.site.register(Armwrestler)
+admin.site.register(Competition)
---
+++ on_prot/main/forms.py
@@ -0,0 +1,7 @@
+from django.forms import forms
+
+
+class WinnerForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+
